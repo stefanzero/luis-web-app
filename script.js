23e4892681968b7f8a3bb77484e83cf69f875559
LuisR <100401133+MannyR00@users.noreply.github.com>
2025-11-26 18:21:53 -0800

    Fetched from airtable

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// function for our list view
+async function getAllRecords() {
+  let getResultElement = document.getElementById("contacts");
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patLtkbXakgkT1gFZ.6b9484757dadc3f47ed82a9ff56bd5882c082f0a7f1bcdba4d70748365394363`,
+    },
+  };
+
+  await fetch(
+    `https://api.airtable.com/v0/appv9pErMJsyQ1KU6/Table%201/rec3QDR6m6LHCGXN4`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // response is an object w/ .records array
+    });
+}
+
+getAllRecords();
